42c65e4007782ffbc1d8846f03b84c73ac349d5a
Minsung Kim <mskim4048@naver.com>
2025-07-11 11:06:17 +0900

    Initial Commit

---
+++ SeSAC7Week3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeSAC7Week3
+//
+//  Created by 김민성 on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(1)
+        print(2)
+        print(3)
+    }
+
+
+}
+
